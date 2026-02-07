5c24126a7a647d3a926b2f726e296527f37292d0
costin-vasilescu <tiberiu.vas1@gmail.com>
2026-02-08 01:05:14 +0200

    Changed opencode keybind in lazyvim plugin

--- .config/lazyvim/lua/plugins/opencode.lua
+++ .config/lazyvim/lua/plugins/opencode.lua
@@ -4,13 +4,62 @@ return {
     { "folke/snacks.nvim", opts = { input = {}, picker = {}, terminal = {} } },
   },
   keys = {
-    { "<C-a>", function() require("opencode").ask("@this: ", { submit = true }) end, desc = "Ask opencode…", mode = { "n", "x" } },
-    { "<C-x>", function() require("opencode").select() end, desc = "Execute opencode action…", mode = { "n", "x" } },
-    { "<C-.>", function() require("opencode").toggle() end, desc = "Toggle opencode", mode = { "n", "t" } },
-    { "go", function() return require("opencode").operator("@this ") end, desc = "Add range to opencode", mode = { "n", "x" }, expr = true },
-    { "goo", function() return require("opencode").operator("@this ") .. "_" end, desc = "Add line to opencode", mode = "n", expr = true },
-    { "<S-C-u>", function() require("opencode").command("session.half.page.up") end, desc = "Scroll opencode up" },
-    { "<S-C-d>", function() require("opencode").command("session.half.page.down") end, desc = "Scroll opencode down" },
+    {
+      "<C-a>",
+      function()
+        require("opencode").ask("@this: ", { submit = true })
+      end,
+      desc = "Ask opencode…",
+      mode = { "n", "x" },
+    },
+    {
+      "<C-x>",
+      function()
+        require("opencode").select()
+      end,
+      desc = "Execute opencode action…",
+      mode = { "n", "x" },
+    },
+    {
+      "<C-\\`>",
+      function()
+        require("opencode").toggle()
+      end,
+      desc = "Toggle opencode",
+      mode = { "n", "t" },
+    },
+    {
+      "go",
+      function()
+        return require("opencode").operator("@this ")
+      end,
+      desc = "Add range to opencode",
+      mode = { "n", "x" },
+      expr = true,
+    },
+    {
+      "goo",
+      function()
+        return require("opencode").operator("@this ") .. "_"
+      end,
+      desc = "Add line to opencode",
+      mode = "n",
+      expr = true,
+    },
+    {
+      "<S-C-u>",
+      function()
+        require("opencode").command("session.half.page.up")
+      end,
+      desc = "Scroll opencode up",
+    },
+    {
+      "<S-C-d>",
+      function()
+        require("opencode").command("session.half.page.down")
+      end,
+      desc = "Scroll opencode down",
+    },
   },
   config = function()
     ---@type opencode.Opts
