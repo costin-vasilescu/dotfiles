3791a1d2aab61e9556cce4c0c70fe267d4291bd8
costin-vasilescu <tiberiu.vas1@gmail.com>
2026-02-22 00:18:19 +0200

    Fix sniprun config

--- .config/lazyvim/lua/plugins/sniprun.lua
+++ .config/lazyvim/lua/plugins/sniprun.lua
@@ -1,26 +1,27 @@
 return {
-  {
-    "folke/which-key.nvim",
-    opts = {
-      spec = {
-        { "<leader>r", group = "run" },
-      },
-    },
-  },
-  {
-    "michaelb/sniprun",
-    build = "sh install.sh 1",
-    opts = {
-      display = {
-        "LongTempFloatingWindow",
-        "VirtualTextOk",
-      },
-      repl_enable = { "Python3_fifo" },
-    },
-    keys = {
-      { "<F5>", "<cmd>SnipRun<cr>", mode = "n", desc = "Run snippet" },
-      { "<F5>", ":'<,'>SnipRun<cr>", mode = "v", desc = "Run selection" },
-      { "<leader>cc", "<cmd>SnipClose<cr>", desc = "Close sniprun" },
-    },
-  },
+	{
+		"folke/which-key.nvim",
+		opts = {
+			spec = {
+				{ "<leader>r", group = "run" },
+			},
+		},
+	},
+	{
+		"michaelb/sniprun",
+		build = "sh install.sh 1",
+		opts = {
+			display = {
+				"LongTempFloatingWindow",
+				"VirtualTextOk",
+			},
+			repl_enable = { "Python3_fifo" },
+		},
+		keys = {
+			{ "<F5>", "<cmd>SnipRun<cr>", mode = "n", desc = "Run current line" },
+			{ "<F5>", ":'<,'>SnipRun<cr>", mode = "v", desc = "Run selection" },
+			{ "<leader>cc", "<cmd>SnipClose<cr>", desc = "Close sniprun" },
+			{ "<leader>cC", "<cmd>SnipReset<cr>", desc = "Reset sniprun" },
+		},
+	},
 }
