52178e192e6d22ee54c05247b75cfee2c154b8ca
costin-vasilescu <128258482+costin-vasilescu@users.noreply.github.com>
2026-08-12 20:55:58 +0300

    Update keymaps.lua

--- lazyvim/.config/lazyvim/lua/config/keymaps.lua
+++ lazyvim/.config/lazyvim/lua/config/keymaps.lua
@@ -13,3 +13,13 @@ vim.keymap.set("n", "<leader>cL", function()
     vim.notify("LSP started", vim.log.levels.INFO)
   end
 end, { desc = "Toggle LSP" })
+
+-- Disable Snacks floating terminal keymaps
+vim.keymap.del("n", "<leader>fT")
+vim.keymap.del("n", "<leader>ft")
+vim.keymap.del({ "n", "t" }, "<c-/>")
+vim.keymap.del({ "n", "t" }, "<c-_>")
+
+-- Enable Toggleterm floating and tab terminals
+vim.keymap.set({ "n", "t" }, "<c-_>", "<cmd>1ToggleTerm direction=float<cr>", { desc = "Toggle Terminal (Float)" })
+vim.keymap.set({"n", "t" }, "<c-\\>", "<cmd>2ToggleTerm direction=tab<cr>", { desc = "Toggle Terminal (Tab)" })
