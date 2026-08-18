aa57763ce199ce3d64596773ff39eb8aac9c963c
costin-vasilescu <tiberiu.vas1@gmail.com>
2026-08-19 00:00:46 +0300

    Update lazyvim keymaps

--- lazyvim/.config/lazyvim/lua/config/keymaps.lua
+++ lazyvim/.config/lazyvim/lua/config/keymaps.lua
@@ -2,18 +2,6 @@
 -- Default keymaps that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/keymaps.lua
 -- Add any additional keymaps here
 
--- Toggle LSP
-vim.keymap.set("n", "<leader>cL", function()
-	local clients = vim.lsp.get_clients({ bufnr = 0 })
-	if #clients > 0 then
-		vim.cmd("LspStop")
-		vim.notify("LSP stopped", vim.log.levels.INFO)
-	else
-		vim.cmd("LspStart")
-		vim.notify("LSP started", vim.log.levels.INFO)
-	end
-end, { desc = "Toggle LSP" })
-
 -- Disable Snacks floating terminal keymaps
 vim.keymap.del("n", "<leader>fT")
 vim.keymap.del("n", "<leader>ft")
