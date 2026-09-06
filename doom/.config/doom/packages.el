954e78be1b9e879fc61443c57e8894cc51f4d90c
costin-vasilescu <tiberiu.vas1@gmail.com>
2026-09-06 16:05:10 +0300

    doom: add pylish

--- doom/.config/doom/packages.el
+++ doom/.config/doom/packages.el
@@ -51,3 +51,6 @@
 ;; (unpin! pinned-package another-pinned-package)
 ;; ...Or *all* packages (NOT RECOMMENDED; will likely break things)
 ;; (unpin! t)
+
+(package! pilish
+  :recipe (:host github :repo "dnouri/pilish"))
